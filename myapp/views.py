cc3f464c08a9fb7cd895472c543b3a8e49d5d4cd
Opad Yusuf <jjyusuf@gmail.com>
2021-10-13 08:36:12 +0300

    added docker details

--- myapp/views.py
+++ myapp/views.py
@@ -5,7 +5,7 @@ from rest_framework import views, status
 from rest_framework.response import Response
 
 from .serializers import CommentSerializer
-from myapp.tasks import send_email
+from myapp.tasks import send_email, send_review_email_task
 
 class CommentView(views.APIView):
 
@@ -15,6 +15,7 @@ class CommentView(views.APIView):
     def post(self, request, format=None):
         serializer = CommentSerializer(data=request.data)
         if serializer.is_valid():
-            send_email.delay(serializer.validated_data['email'], serializer.validated_data['subject'], serializer.validated_data['message'])
+            #send_email.delay(serializer.validated_data['email'], serializer.validated_data['subject'], serializer.validated_data['message'])
+            send_review_email_task.delay('Somebody', serializer.validated_data['email'], serializer.validated_data['message'])
             return Response({'message':'Email sent successfully'}, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
